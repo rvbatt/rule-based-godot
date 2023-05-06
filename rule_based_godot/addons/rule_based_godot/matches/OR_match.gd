8e2a0b36dab7aafdc5db0e641b4a4ecb4c18d681
Rodrigo Volpe Battistin <rv.battistin@usp.br>
2023-05-06 09:47:04 -0300

    Implement ORMatch
    Signed-off-by: Rodrigo Volpe Battistin <rv.battistin@usp.br>

---
+++ rule_based_godot/addons/rule_based_godot/matches/OR_match.gd
@@ -0,0 +1,20 @@
+class_name ORMatch
+extends AbstractMatch
+
+@export var subconditions: Array[AbstractMatch] = []
+# We can't set a default value for subconditions in _init,
+# because the resource tree is constructed from bottom up,
+# so its children will be already initialized
+
+func set_system_node(system_node: Node) -> void:
+	for condition in subconditions:
+		if condition == null: continue
+		condition.set_system_node(system_node)
+
+func is_satisfied() -> bool:
+	for condition in subconditions:
+		if condition == null: continue
+		if condition.is_satisfied():
+			return true
+
+	return false
