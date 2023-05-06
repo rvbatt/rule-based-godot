7dae6af174ed007cc1e1a3a033ca5fd0323fc49d
Rodrigo Volpe Battistin <rv.battistin@usp.br>
2023-05-06 13:10:22 -0300

    Allow String and Numeric matches to call methods
    - Use the same interface on both to keep consistency
    - By allowing method calling, the interface becomes as versatile as
    possible
    
    Signed-off-by: Rodrigo Volpe Battistin <rv.battistin@usp.br>

--- project/addons/rule_based_godot/matches/numeric_match.gd
+++ project/addons/rule_based_godot/matches/numeric_match.gd
@@ -2,7 +2,8 @@ class_name NumericMatch
 extends AbstractMatch
 
 @export var test_node_path: NodePath
-@export var test_number_property: StringName
+@export var property_or_method: StringName
+@export var method_arguments: Array
 @export var min_value: float
 @export var max_value: float
 
@@ -12,9 +13,14 @@ func is_satisfied() -> bool:
 		print_debug("Invalid NumericMatch node")
 		return false
 
-	var test_number = test_node.get(test_number_property)
+	var test_number = null
+	if property_or_method in test_node:
+		test_number = test_node.get(property_or_method)
+	elif test_node.has_method(property_or_method):
+		test_number = test_node.callv(property_or_method, method_arguments)
+
 	if test_number == null:
-		print_debug("Invalid NumericMatch property")
+		print_debug("Invalid NumericMatch property or method")
 		return false
 
 	return (min_value <= test_number) and (test_number <= max_value)
--- project/addons/rule_based_godot/matches/string_match.gd
+++ project/addons/rule_based_godot/matches/string_match.gd
@@ -2,7 +2,8 @@ class_name StringMatch
 extends AbstractMatch
 
 @export var test_node_path: NodePath
-@export var test_string_property: StringName
+@export var property_or_method: StringName
+@export var method_arguments: Array
 @export var string_value: String
 
 func is_satisfied() -> bool:
@@ -11,9 +12,15 @@ func is_satisfied() -> bool:
 		print_debug("Invalid StringMatch node")
 		return false
 
-	var test_string = test_node.get(test_string_property)
+	var test_string = null
+	if property_or_method in test_node:
+		test_string = test_node.get(property_or_method)
+	elif test_node.has_method(property_or_method):
+		test_string = test_node.callv(property_or_method, method_arguments)
+
 	if test_string == null:
-		print_debug("Invalid StringMatch property")
+		print_debug("Invalid StringMatch property or method")
 		return false
 
 	return test_string == string_value
+	
