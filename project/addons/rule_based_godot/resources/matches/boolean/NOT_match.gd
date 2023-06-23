63df47b352f556f5e9bc3fc73d190c0f69816687
Rodrigo Volpe Battistin <rv.battistin@usp.br>
2023-06-23 08:09:26 -0300

    Fix NOTMatch not building internal condition
    Signed-off-by: Rodrigo Volpe Battistin <rv.battistin@usp.br>

--- project/addons/rule_based_godot/resources/matches/boolean/NOT_match.gd
+++ project/addons/rule_based_godot/resources/matches/boolean/NOT_match.gd
@@ -7,18 +7,18 @@ func setup(system_node: Node) -> void:
 	if negated_condition != null:
 		negated_condition.setup(system_node)
 
-func is_satisfied() -> bool:
-	if negated_condition == null:
-		print_debug("Empty NOTMatch")
-		return false
-	else:
-		return not (negated_condition.is_satisfied())
-
 func to_json_string() -> String:
 	# ["NOT", condition]
 	return '["NOT", ' + negated_condition.to_json_string() + ']'
 
 func build_from_repr(json_repr) -> void:
 	# ["NOT", condition]
-	var condition = RuleFactory.create_match(json_repr[1])
-	condition.setup(_system_node)
+	negated_condition = RuleFactory.create_match(json_repr[1])
+	negated_condition.setup(_system_node)
+
+func is_satisfied() -> bool:
+	if negated_condition == null:
+		print_debug("Empty NOTMatch")
+		return false
+	else:
+		return not (negated_condition.is_satisfied())
