b7299f6b99a9e493a3148cab4d82ede78ca70520
Rodrigo Volpe Battistin <rv.battistin@usp.br>
2023-07-10 15:02:12 -0300

    Test unification with StringMatch and SetProperty
    - The node that matched the string will be used by the actions
    - Keep the Resource Tree usability by exporting a bool to check if the
    node path is a wildcard or not
    
    Signed-off-by: Rodrigo Volpe Battistin <rv.battistin@usp.br>

--- project/addons/rule_based_godot/resources/actions/set_property_action.gd
+++ project/addons/rule_based_godot/resources/actions/set_property_action.gd
@@ -1,27 +1,44 @@
 class_name SetPropertyAction
 extends AbstractAction
 
-@export var setter_path: NodePath
+@export_group("Test NodePath", "uni")
+@export var uni_is_wildcard: bool = false
+@export var uni_identifier: StringName
+@export var uni_setter_path: NodePath
+
 @export var property: StringName
 @export var type: StringName
 @export var value: Array
 
 static func json_format() -> String:
-	return '["Set Property", "setter_node", "property", "type", "value"]'
+	return '["Set Property", "?var|node", "property", "type", "value"]'
 
 func to_json_string() -> String:
 	# Follows json_format
-	return JSON.stringify(["Set", setter_path, property, type, value[0]])
+	var var_or_path = '?' + uni_identifier if uni_is_wildcard else str(uni_setter_path)
+	return JSON.stringify(["Set", var_or_path, property, type, value[0]])
 
 func build_from_repr(json_repr) -> void:
 	# Follows json_format
-	setter_path = NodePath(json_repr[1])
+	if json_repr[1].begins_with('?'):
+		uni_is_wildcard = true
+		uni_identifier = json_repr[1].trim_prefix('?')
+		uni_setter_path = ^"."
+	else:
+		uni_is_wildcard = false
+		uni_identifier = ""
+		uni_setter_path = NodePath(json_repr[1])
 	property = json_repr[2]
 	type = json_repr[3]
-	value = [eval_value(type, json_repr[4])]
+	value = [self.eval_value(type, json_repr[4])]
 
-func trigger() -> Variant:
-	var setter: Node = _system_node.get_node(setter_path)
+func trigger(bindings: Dictionary) -> Variant:
+	print("Trigger set")
+	var setter: Node
+	if uni_is_wildcard:
+		setter = _system_node.get_node(bindings.get(uni_identifier, ^"."))
+	else:
+		setter = _system_node.get_node(uni_setter_path)
 	if setter == null:
 		print_debug("Invalid SetPropertyAction setter")
 		return null
--- project/addons/rule_based_godot/resources/matches/string_match.gd
+++ project/addons/rule_based_godot/resources/matches/string_match.gd
@@ -1,35 +1,59 @@
-
 class_name StringMatch
 extends AbstractMatch
 
-@export var test_node_path: NodePath
+@export_group("Test NodePath", "uni")
+@export var uni_is_wildcard: bool = false
+@export var uni_identifier: StringName
+@export var uni_test_node_path: NodePath
+
+@export var string_value: String
 @export var property_or_method: StringName
 @export var method_arguments: Dictionary #Type -> value
-@export var string_value: String
 
 static func json_format() -> String:
-	return '["String", "value", "test_node", "property_or_method", {"types": "arguments"}]'
+	return '["String", "value", "?var|node", "property_or_method", {"types": "arguments"}]'
 
 func to_json_string() -> String:
 	# ["String", value, test_node, property_or_method, {types: arguments}]
+	var var_or_path = '?' + uni_identifier if uni_is_wildcard else str(uni_test_node_path)
 	return JSON.stringify(
-		["String", string_value, test_node_path, property_or_method, method_arguments]
+		["String", string_value, var_or_path, property_or_method, method_arguments]
 	)
 
 func build_from_repr(json_repr) -> void:
 	# ["String", value, test_node, property_or_method, {types: arguments}]
 	string_value = json_repr[1]
-	test_node_path = NodePath(json_repr[2])
+	if json_repr[2].begins_with('?'):
+		uni_is_wildcard = true
+		uni_identifier = json_repr[2].trim_prefix('?')
+		uni_test_node_path = ^"."
+	else:
+		uni_is_wildcard = false
+		uni_identifier = ""
+		uni_test_node_path = NodePath(json_repr[2])
 	property_or_method = json_repr[3]
 	method_arguments = eval_arguments(json_repr[4])
 
-func is_satisfied() -> bool:
-	var test_node = _system_node.get_node(test_node_path)
+func is_satisfied(bindings: Dictionary) -> bool:
+	if uni_is_wildcard:
+		for child in _system_node.get_children(true):
+			print(child.name)
+			if _get_string_value(child) == string_value:
+				print("Try to add binding")
+
+				bindings[uni_identifier] = _system_node.get_path_to(child)
+				return true
+		return false
+
+	var test_node = _system_node.get_node(uni_test_node_path)
 	if test_node == null:
 		print_debug("Invalid StringMatch node")
 		return false
+	return _get_string_value(test_node) == string_value
 
+func _get_string_value(test_node: Node) -> Variant:
 	var test_string = null
+
 	if property_or_method in test_node:
 		test_string = test_node.get(property_or_method)
 	elif test_node.has_method(property_or_method):
@@ -37,6 +61,4 @@ func is_satisfied() -> bool:
 
 	if test_string == null:
 		print_debug("Invalid StringMatch property or method")
-		return false
-
-	return test_string == string_value
+	return test_string
