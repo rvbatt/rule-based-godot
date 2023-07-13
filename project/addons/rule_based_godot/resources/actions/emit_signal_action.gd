6ce09f3af84c4b4d114dc2db8d44500109a45396
Rodrigo Volpe Battistin <rv.battistin@usp.br>
2023-07-13 10:49:42 -0300

    Adapt EmitSignalAction to have wildcard ActionNode
    - Now we can make any node emit a custom user signal
    
    Signed-off-by: Rodrigo Volpe Battistin <rv.battistin@usp.br>

--- project/addons/rule_based_godot/resources/actions/emit_signal_action.gd
+++ project/addons/rule_based_godot/resources/actions/emit_signal_action.gd
@@ -1,22 +1,23 @@
+@tool
 class_name EmitSignalAction
 extends AbstractAction
 
 @export var signal_name: StringName
 
 static func json_format() -> String:
-	return '["Emit Signal", "signal_name"]'
-
-func setup(system_node: Node) -> void:
-	_system_node = system_node
-	_system_node.add_user_signal(signal_name)
+	return '["Emit Signal", "?var|node", signal_name"]'
 
 func to_json_string() -> String:
 	# Follows json_format
-	return '["Emit", "' + signal_name + '"]'
+	return JSON.stringify(["Emit Signal", _var_or_path_string(), signal_name])
 
 func build_from_repr(json_repr) -> void:
 	# Follows json_format
-	signal_name = json_repr[1]
+	_build_var_or_path(json_repr[1])
+	signal_name = json_repr[2]
 
 func trigger(bindings: Dictionary) -> Variant:
+	var action_node = _get_action_node(bindings)
+	if not action_node.has_user_signal(signal_name):
+		action_node.add_user_signal(signal_name)
 	return _system_node.emit_signal(signal_name)
