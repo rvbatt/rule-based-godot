ac69401793570e5e96ad489848cc00763eb2ea4e
Rodrigo Volpe Battistin <rv.battistin@usp.br>
2023-08-07 16:24:36 -0300

    Turn AbstractMatch into a configurable superclass
    Absorb the functionalities of VariableTarget and ObtainInfoMatch to
    avoid a deep and complex class hierarchy
    - Use PROPERTY_USAGE_GROUP to create dynamic groups in the Inspector,
    which can be added or removed by setting the same name variable to true
    or false, respectively
    - Implement _get_property_list() to dynamically change the properties
    shown in the Inspector, always remembering to notify when the property
    list changed
    
    Create auxiliary functions to setup(), so that subclasses dont need to
    override this method. This hides the complexity from users that want to
    create new matches
    
    Expand Template Method on is_satisfied() to accomodate when some data
    is extracted or retrieved from the target node
    - When Data_Extraction = true, there is no need to implement _get_data(),
    since one of the premade options will be used
    - When Data_Retrieval = true, _get_data() needs to return the data that
    will be added to the bindings
    
    Add the option to retrieve data, for when a match is valid in an interval
    and we want to use the exact value achieved somewhere. This is done by
    defining a variable that can be accessed with the '?' prefix
    
    Automate JSON-format string generation and self building
    - Use new variables match_id and repr_vars, that must be initialized in
    every class
    
    Signed-off-by: Rodrigo Volpe Battistin <rv.battistin@usp.br>

--- addons/rule_based_godot/resources/matches/abstract_match.gd
+++ addons/rule_based_godot/resources/matches/abstract_match.gd
@@ -2,7 +2,346 @@ class_name AbstractMatch
 extends RuleBasedResource
 # Abstract class for condition components
 
+var match_id: StringName # ID for json format
+var repr_vars: Array[StringName] # variables of json format
+
+func _get_property_list():
+	var properties = _target_properties()
+	properties.append_array(_extraction_properties())
+	properties.append_array(_retrieval_properties())
+	return properties
+
+######################### The node that will be tested #########################
+var Target_Node: bool = true:
+	set(value):
+		Target_Node = value
+		notify_property_list_changed()
+# Group: Target_Node, prefix: target
+var target_is_wildcard: bool = false:
+	set(value):
+		target_is_wildcard = value
+		if value:
+			retrieval_should_retrieve = false
+		notify_property_list_changed()
+var target_search_groups: PackedStringArray = []
+var target_identifier: StringName = ""
+var target_path: NodePath = ^""
+
+var _target_node: Node # internal reference
+
+func _target_properties() -> Array[Dictionary]:
+	var properties: Array[Dictionary] = [
+		{"name": "Target_Node",
+		"type": TYPE_BOOL,
+		"usage": PROPERTY_USAGE_GROUP,
+		"hint_string": "target"}
+	]
+	if not Target_Node: return properties
+
+	properties.append(
+		{"name": "target_is_wildcard",
+		"type": TYPE_BOOL,
+		"usage": PROPERTY_USAGE_DEFAULT}
+	)
+	if target_is_wildcard:
+		properties.append_array([
+			{"name": "target_search_groups",
+			"type": TYPE_PACKED_STRING_ARRAY,
+			"usage": PROPERTY_USAGE_ARRAY},
+			{"name": "target_identifier",
+			"type": TYPE_STRING_NAME,
+			"usage": PROPERTY_USAGE_DEFAULT}
+		])
+	else:
+		properties.append(
+			{"name": "target_path",
+			"type": TYPE_NODE_PATH,
+			"usage": PROPERTY_USAGE_DEFAULT,
+			"hint": PROPERTY_HINT_NODE_PATH_TO_EDITED_NODE}
+		)
+	return properties
+
+################# The data that will be extracted from the node ################
+var Data_Extraction: bool = false:
+	set(value):
+		Data_Extraction = value
+		notify_property_list_changed()
+# Group: Data_Extraction, prefix: extraction
+enum ExtractionType {PROPERTY, METHOD}
+var extraction_type: ExtractionType = ExtractionType.PROPERTY:
+	set(value):
+		extraction_type = value
+		notify_property_list_changed()
+var extraction_property: StringName = ""
+var extraction_method: StringName = ""
+var extraction_arguments: Array = []
+
+func _extraction_properties() -> Array[Dictionary]:
+	var properties: Array[Dictionary] = [
+		{"name": "Data_Extraction",
+		"type": TYPE_BOOL,
+		"usage": PROPERTY_USAGE_GROUP,
+		"hint_string": "extraction"}
+	]
+	if not Data_Extraction: return properties
+
+	var extract_types := ""
+	for type in ExtractionType:
+		extract_types += type.capitalize() + ","
+	extract_types = extract_types.trim_suffix(",")
+	properties.append(
+		{"name": "extraction_type",
+		"type": TYPE_INT,
+		"usage": PROPERTY_USAGE_DEFAULT,
+		"hint": PROPERTY_HINT_ENUM,
+		"hint_string": extract_types}
+	)
+
+	match extraction_type:
+		ExtractionType.PROPERTY:
+			properties.append(
+				{"name": "extraction_property",
+				"type": TYPE_STRING_NAME,
+				"usage": PROPERTY_USAGE_DEFAULT}
+			)
+		ExtractionType.METHOD:
+			properties.append_array([
+			{"name": "extraction_method",
+			"type": TYPE_STRING_NAME,
+			"usage": PROPERTY_USAGE_DEFAULT},
+			{"name": "extraction_arguments",
+			"type": TYPE_ARRAY,
+			"usage": PROPERTY_USAGE_DEFAULT}
+			])
+
+	return properties
+
+####################### Retrieve data from verification ########################
+var Data_Retrieval: bool = true:
+	set(value):
+		Data_Retrieval = value
+		notify_property_list_changed()
+# Group: Data_Retrieval, prefix: retrieval
+var retrieval_should_retrieve: bool = false:
+	set(value):
+		retrieval_should_retrieve = value
+		if value:
+			target_is_wildcard = false
+		notify_property_list_changed()
+var retrieval_variable: StringName = ""
+
+func _retrieval_properties() -> Array[Dictionary]:
+	var properties: Array[Dictionary] = [
+		{"name": "Data_Retrieval",
+		"type": TYPE_BOOL,
+		"usage": PROPERTY_USAGE_GROUP,
+		"hint_string": "retrieval"}
+	]
+	if not Data_Retrieval: return properties
+	
+	properties.append(
+		{"name": "retrieval_should_retrieve",
+		"type": TYPE_BOOL,
+		"usage": PROPERTY_USAGE_DEFAULT}
+	)
+	if retrieval_should_retrieve:
+		properties.append(
+			{"name": "retrieval_variable",
+			"type": TYPE_STRING_NAME,
+			"usage": PROPERTY_USAGE_DEFAULT}
+		)
+	return properties
+
+######################### Setup on system node ready ###########################
+func setup(system_node: Node) -> void:
+	if system_node == null:
+		print_debug("Setup with null system node")
+		return
+	_system_node = system_node
+	if not target_is_wildcard:
+		_target_node = system_node.get_node(target_path)
+	for path in _preset_paths:
+		set(_preset_paths[path], system_node.get_node(get(path)))
+	for connection in _preset_connections:
+		var node: Node = get(connection[0])
+		node.connect(connection[1], _preset_connections[connection])
+
+var _preset_paths: Dictionary = {} # path_var -> node_var
+func preset_node_path(path_variable: StringName,
+		node_variable: StringName) -> void:
+	_preset_paths[path_variable] = node_variable
+
+var _preset_connections: Dictionary = {} #[node_var, signal_name] -> func
+func pre_connect(node_variable: StringName, signal_name: StringName,
+		function: Callable):
+	var node_and_signal = [node_variable, signal_name]
+	_preset_connections[node_and_signal] = function
+
+############################# Is match satisfied ###############################
 func is_satisfied(bindings: Dictionary) -> bool:
-	# Abstract Method
-	push_error("Abstract Method call")
+	# Uses Template method
+	if target_is_wildcard:
+		var candidates = bindings.get(target_identifier) \
+			if bindings.has(target_identifier) \
+			else _get_candidates()
+
+		var valid_candidates := []
+		for candidate in candidates:
+			if _node_satisfies_match(candidate, bindings):
+				valid_candidates.append(candidate)
+
+		bindings[target_identifier] = valid_candidates
+		return not valid_candidates.is_empty()
+
+	return _node_satisfies_match(_target_node, bindings)
+
+func _get_candidates() -> Array[Node]:
+	# If no group is provided, default to descendants of system node
+	if target_search_groups.is_empty():
+		return _system_node.get_children(true)
+
+	var scene: SceneTree = _system_node.get_tree()
+	var candidates: Array[Node] = []
+	for group in target_search_groups:
+		candidates.append_array(scene.get_nodes_in_group(group))
+	return candidates
+
+func is_in_search_groups(node: Node) -> bool:
+	# If there is no group, allow any node
+	if target_search_groups.is_empty(): return true
+
+	for group in target_search_groups:
+		if node.is_in_group(group): return true
+
 	return false
+
+func _node_satisfies_match(target_node: Node, bindings: Dictionary) -> bool:
+	var data = _get_data(target_node)
+	if retrieval_should_retrieve:
+		bindings[retrieval_variable] = data
+	return _data_satisfies_match(data)
+
+func _data_satisfies_match(data: Variant) -> bool:
+	push_error("Abstract Method")
+	return false
+
+func _get_data(target_node: Node) -> Variant:
+	if target_node == null:
+		print_debug("Invalid node")
+		return null
+
+	if not Data_Extraction:
+		push_error(
+			"_get_data(node) should be implemented if Data_Extraction = false")
+		return null
+
+	match extraction_type:
+		ExtractionType.PROPERTY:
+			if not extraction_property in target_node:
+				print_debug("Invalid property")
+				return null
+			return target_node.get(extraction_property)
+
+		ExtractionType.METHOD:
+			if not target_node.has_method(extraction_method):
+				print_debug("Invalid method")
+				return null
+			return target_node.callv(extraction_method, extraction_arguments)
+
+		_:
+			print_debug("Invalid DataExtraction type")
+			return null
+
+################################ JSON format ###################################
+func to_json_repr() -> Variant:
+	# ["ID", (?data,) vars..., "?wild"|"target", ("prop" | "method", [args])]
+	var json_array = [match_id]
+	if retrieval_should_retrieve:
+		json_array.append(var_to_str('?' + retrieval_variable))
+
+	for variable in repr_vars:
+		json_array.append(var_to_str(get(variable)))
+
+	if target_is_wildcard:
+		json_array.append(var_to_str('?' + target_identifier))
+	else:
+		json_array.append(var_to_str(target_path))
+
+	if Data_Extraction:
+		match extraction_type:
+			ExtractionType.PROPERTY:
+				json_array.append(var_to_str(extraction_property))
+			ExtractionType.METHOD:
+				json_array.append_array([
+					var_to_str(extraction_method),
+					var_to_str(extraction_arguments)
+				])
+
+	return json_array
+
+func build_from_repr(json_repr: Array) -> void:
+	var offset = 1
+	var first_param = str_to_var(json_repr[1])
+	if first_param is String and first_param.begins_with('?'):
+		Data_Retrieval = true
+		retrieval_should_retrieve = true
+		retrieval_variable = first_param.trim_prefix('?')
+		offset = 2
+
+	var num_vars = repr_vars.size()
+	for i in range(num_vars):
+		set(repr_vars[i], str_to_var(json_repr[i+offset]))
+
+	var var_or_path = str_to_var(json_repr[offset + num_vars])
+	if var_or_path is String and var_or_path.begins_with('?'):
+		target_is_wildcard = true
+		target_identifier = var_or_path.trim_prefix('?')
+	elif var_or_path is NodePath:
+		target_is_wildcard = false
+		target_path = var_or_path
+
+	var num_data_params = json_repr.size() - num_vars - offset - 1
+	if num_data_params == 1:
+		Data_Extraction = true
+		extraction_type = ExtractionType.PROPERTY
+		extraction_property = str_to_var(json_repr[-1])
+	elif num_data_params == 2:
+		Data_Extraction = true
+		extraction_type = ExtractionType.METHOD
+		extraction_method = str_to_var(json_repr[-2])
+		extraction_arguments = str_to_var(json_repr[-1])
+
+# TODO: remove or move the functions below
+func _build_var_or_path(var_or_path: String) -> void:
+	# Auxilary function
+	if var_or_path.begins_with('?'):
+		target_is_wildcard = true
+		target_identifier = var_or_path.trim_prefix('?')
+		target_path = ^""
+	else:
+		target_is_wildcard = false
+		target_identifier = ""
+		target_path = NodePath(var_or_path)
+
+func _var_or_path_string() -> String:
+	# Auxilary function
+	return '?' + target_identifier if target_is_wildcard else var_to_str(target_path)
+
+func _build_prop_or_method(json_repr: Array, start_index: int) -> void:
+	# Auxilary function
+	if json_repr.size() == start_index + 1: # only property
+		extraction_type = 0
+		extraction_property = json_repr[start_index]
+		extraction_method = ""
+		extraction_arguments = []
+	else:
+		extraction_type = 1
+		extraction_property = ""
+		extraction_method = json_repr[start_index]
+		extraction_arguments = _eval_arguments(json_repr[start_index + 1])
+
+func _prop_or_method_array() -> Array:
+	# Auxilary function
+	return [extraction_property] \
+		if extraction_type == 0 \
+		else [extraction_method, extraction_arguments]
