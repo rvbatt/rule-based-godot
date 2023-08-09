76bf16165bb9565b0df74fc0ac7d8dca23909478
Rodrigo Volpe Battistin <rv.battistin@usp.br>
2023-08-09 15:47:25 -0300

    Allow arguments in signals from EmitSignalAction
    Also preset the signals when the agent nodes are constant (not wildcard)
    
    Signed-off-by: Rodrigo Volpe Battistin <rv.battistin@usp.br>

--- addons/rule_based_godot/resources/actions/abstract_action.gd
+++ addons/rule_based_godot/resources/actions/abstract_action.gd
@@ -15,7 +15,6 @@ var agent_is_wildcard: bool = false:
 		notify_property_list_changed()
 var agent_identifier: StringName = ""
 var agent_path: NodePath = ^"."
-
 var _agent_node: Node
 
 func _get_property_list():
@@ -47,10 +46,29 @@ func _get_property_list():
 		)
 	return properties
 
+var _preset_signals = {} # name_var -> param_to_type_var
+func pre_add_signal(name_var: StringName, param_to_type_var: StringName) -> void:
+	_preset_signals[name_var] = param_to_type_var
+
+func signal_param_array(param_to_type: Dictionary) -> Array[Dictionary]:
+	var params: Array[Dictionary] = []
+	for name in param_to_type:
+		params.append(
+			{"name": name, "type": typeof(param_to_type[name])}
+		)
+	return params
+
 func setup(system_node: Node) -> void:
 	_system_node = system_node
-	if not agent_is_wildcard:
-		_agent_node = system_node.get_node(agent_path)
+	if agent_is_wildcard: return
+
+	_agent_node = system_node.get_node(agent_path)
+	for name_var in _preset_signals:
+		var signal_name = get(name_var)
+		if _agent_node.has_signal(signal_name): continue
+
+		var params = signal_param_array(get(_preset_signals[name_var]))
+		_agent_node.add_user_signal(get(name_var), params)
 
 func trigger(bindings: Dictionary) -> Array:
 	# Uses Template Method
--- addons/rule_based_godot/resources/actions/emit_signal_action.gd
+++ addons/rule_based_godot/resources/actions/emit_signal_action.gd
@@ -3,15 +3,23 @@ class_name EmitSignalAction
 extends AbstractAction
 
 @export var signal_name: StringName
+@export var parameter_to_type: Dictionary = {}
+@export var arguments: Array = []# same order as dict
 
 static func json_format() -> String:
 	return '["EmitSignal", "?var|node", "signal"]'
 
 func _init():
 	action_id = "EmitSignal"
-	repr_vars = ["signal_name"]
+	repr_vars = ["signal_name", "parameter_to_type", "arguments"]
+	pre_add_signal("signal_name", "parameter_to_type")
 
 func _result_from_agent(agent: Node, bindings: Dictionary) -> Variant:
-	if not agent.has_user_signal(signal_name):
-		agent.add_user_signal(signal_name)
-	return agent.emit_signal(signal_name)
+	var args = arguments
+	for i in range(args.size()):
+		var arg = args[i]
+		if arg is String and arg.begins_with('?'):
+			args[i] = bindings.get(arg.trim_prefix('?'))
+	if not agent.has_signal(signal_name):
+		agent.add_user_signal(signal_name, signal_param_array(parameter_to_type))
+	return agent.emit_signal(signal_name, args)
