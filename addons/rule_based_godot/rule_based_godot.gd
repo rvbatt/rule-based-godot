47648281189a6aa2f7f6bd8e9bcc6f42c9ac8620
Rodrigo Volpe Battistin <rv.battistin@usp.br>
2023-09-20 13:56:10 -0300

    Add automatic iterations for RuleBasedSystem
    Since there are situations where we expect the RuleBasedSystem to iterate
    continuously, the user should not have to manually call the iterate()
    every time. Add options to:
    - Iterate every frame
    - Iterate every X seconds
    
    In order to do that, change the type of the RuleBasedSystem from a
    simple Node to a Timer
    
    Signed-off-by: Rodrigo Volpe Battistin <rv.battistin@usp.br>

--- addons/rule_based_godot/rule_based_godot.gd
+++ addons/rule_based_godot/rule_based_godot.gd
@@ -9,7 +9,7 @@ var inspector_plugin: EditorInspectorPlugin
 
 func _enter_tree():
 	# Initialization of the plugin goes here.
-	add_custom_type("RuleBasedSystem", "Node",
+	add_custom_type("RuleBasedSystem", "Timer",
 		preload("system/rule_based_system.gd"),
 		preload("ruler_icon.png"))
 
--- addons/rule_based_godot/system/rule_based_system.gd
+++ addons/rule_based_godot/system/rule_based_system.gd
@@ -1,24 +1,67 @@
 @icon("../ruler_icon.png")
 class_name RuleBasedSystem
-extends Node
+extends Timer
 # System object that will be placed in scenes
+
+signal iteration_results(results: Array)
+enum IterationUpdate {EVERY_FRAME, ON_TIMER, ON_CALL}
 
 # Identifier for the RulesInspectorPlugin
 @export var _rule_based_godot: StringName = "System"
 
+@export var iteration_update: IterationUpdate = IterationUpdate.ON_TIMER:
+	set = _set_iteration_update
 @export var arbiter: AbstractArbiter = FirstApplicableArbiter.new()
 @export var rule_list := RuleList.new()
+
+var _iterate_every_frame := false
 
 func _ready():
 	if rule_list != null:
 		rule_list.setup(self)
 	print(rule_list.to_json_repr())
+	if iteration_update == IterationUpdate.ON_TIMER:
+		self.connect("timeout", _on_timeout)
+		start()
+
+func _physics_process(_delta):
+	if _iterate_every_frame:
+		iterate()
 
 func iterate() -> Array:
-	var satified_rules = rule_list.satisfied_rules()
+	var results := []
 
+	var satified_rules = rule_list.satisfied_rules()
 	if not satified_rules.is_empty():
 		var selected_rule = arbiter.select_rule_to_trigger(satified_rules)
-		return selected_rule.trigger_actions()
+		results = selected_rule.trigger_actions()
 
-	return []
+	iteration_results.emit(results)
+	return results
+
+func _set_iteration_update(type: IterationUpdate) -> void:
+	match(type):
+		IterationUpdate.EVERY_FRAME:
+			_iterate_every_frame = true
+			if is_connected("timeout", _on_timeout):
+				self.disconnect("timeout", _on_timeout)
+			stop()
+		IterationUpdate.ON_TIMER:
+			_iterate_every_frame = false
+			if not is_connected("timeout", _on_timeout):
+				self.connect("timeout", _on_timeout)
+			start()
+		IterationUpdate.ON_CALL:
+			_iterate_every_frame = false
+			if is_connected("timeout", _on_timeout):
+				self.disconnect("timeout", _on_timeout)
+			stop()
+		_:
+			push_error("Invalid Iteration Update")
+			return
+
+	iteration_update = type
+
+func _on_timeout() -> void:
+	iterate()
+	start()
